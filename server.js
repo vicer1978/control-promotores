ae3895790c0abaf4abf0f2bb56926b8fda66a9d6
Victor Cervantes <vicer1978@gmail.com>
2026-03-26 14:13:03 -0600

    Fix: syntax error in wildcard route for Express compatibility

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.post("/login", async (req, res) => {
 });
 
 // --- REDIRECCIÓN FINAL (Cualquier ruta no encontrada va al Login) ---
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, "public", "login.html"));
 });
 

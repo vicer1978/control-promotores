5632aa3aec60c4acb8eaef73b6412d7a8a29dd01
Victor Cervantes <vicer1978@gmail.com>
2026-03-26 20:02:11 -0600

    se modifico admin.html y user.js

--- models/User.js
+++ models/User.js
@@ -4,14 +4,10 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    // Actualizamos el enum para que coincida con tu frontend
-    role: { 
-        type: String, 
-        enum: ['admin', 'promotor', 'demostradora', 'Demostradora (Fijo)', 'super-admin'],
-        default: 'promotor'
-    },
+    // Eliminamos el enum para que acepte "Promotor (Ruta)" y "Demostradora (Fijo)" sin errores
+    role: { type: String, default: 'Promotor (Ruta)' },
     agencyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Agency' },
-    stores: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Store' }] 
+    stores: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Store' }]
 });
 
 module.exports = mongoose.model('User', UserSchema);

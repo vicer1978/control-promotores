9de3c34c08bb4feb6a0488b54c5ffb65751c6e2e
Victor Cervantes <vicer1978@gmail.com>
2026-04-19 23:29:18 -0600

    se modifico Project.js

--- models/Project.js
+++ models/Project.js
@@ -3,9 +3,12 @@ const mongoose = require("mongoose");
 const ProjectSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String },
+    // AGREGAMOS ESTO:
+    clientId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     agencyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Agency', required: true },
     active: { type: Boolean, default: true },
     createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("Project", ProjectSchema);
+

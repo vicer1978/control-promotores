686ccb3af78d0526d0e4d5119e68e0d1edcd048f
Victor Cervantes <vicer1978@gmail.com>
2026-04-24 10:02:26 -0600

    se modifico Agency.js, super-admin.html, server.js

--- models/Agency.js
+++ models/Agency.js
@@ -1,9 +1,10 @@
 // models/Agency.js
-
 const mongoose = require("mongoose");
 
 const agencySchema = new mongoose.Schema({
   name: { type: String, required: true },
-}, { timestamps: true }); // createdAt y updatedAt automáticamente
+  email: { type: String, lowercase: true, trim: true }, // Para contacto
+  isActive: { type: Boolean, default: true }           // Para el control de estatus
+}, { timestamps: true });
 
 module.exports = mongoose.model("Agency", agencySchema);

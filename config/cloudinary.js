ebac26d7883154bcdd25d32f7ab913a245db1785
Victor Cervantes <vicer1978@gmail.com>
2026-05-10 21:29:00 -0600

    Uso de variables de entorno para Cloudinary

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,24 +1,23 @@
+// android/config/cloudinary.js
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 
+// Usamos process.env para que tome los valores de Render
 cloudinary.config({
-  cloud_name: 'dxfg3pb5d',
-  api_key: '127619153986259',
-  api_secret: '3Ith4U_bQMu7G_0tlZ7J1INUpc8'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
-
-
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'storepulse_reports', // Nombre de la carpeta en Cloudinary
+    folder: 'storepulse_reports',
     allowed_formats: ['jpg', 'png', 'jpeg'],
-    transformation: [{ width: 800, height: 800, crop: 'limit' }] // Optimiza tamaño
+    transformation: [{ width: 800, height: 800, crop: 'limit' }]
   },
 });
 
 const uploadCloud = multer({ storage: storage });
-
 module.exports = uploadCloud;
